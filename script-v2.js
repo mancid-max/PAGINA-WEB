3e2c120b50e2398bea56b5c98771af80cb3bb9ee
mancid-max <man.cid@mohicanojeans.cl>
2026-07-06 13:17:15 -0400

    fix: quitar fila Bota del modal Cole 43

--- script-v2.js
+++ script-v2.js
@@ -3305,7 +3305,6 @@ function renderizarInfoProductoCatalogo43(charList, producto, detallePrecio) {
   [
     crearItemInfoCatalogo43("Tipo", meta43?.tipo),
     crearItemInfoCatalogo43("Tiro", meta43?.tiro),
-    crearItemInfoCatalogo43("Bota", meta43?.bota),
   ].filter(Boolean).forEach((li) => ul.appendChild(li));
 
   if (detallePrecio) {
